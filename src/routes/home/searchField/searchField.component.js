e6252740d73a740432e693d39fc30a0ce39c66d7
Wojciech Panek <wpanek@apptension.com>
2019-09-26 22:50:22 +0200

    Add debounce to searchField

--- src/routes/home/searchField/searchField.component.js
+++ src/routes/home/searchField/searchField.component.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useIntl } from 'react-intl';
 import { Input } from 'antd';
 import { stringify, parse } from 'query-string';
+import { useDebouncedCallback } from 'use-debounce';
 
 import { RepositoriesActions } from '../../../modules/repositories';
 import { Container } from './searchField.styles';
@@ -16,29 +17,23 @@ export const SearchField = () => {
 
   const [value, setValue] = useState(parse(history.location.search).query || '');
 
-  const handleSearch = useCallback(
-    value => {
-      const currentParams = parse(history.location.search);
-      history.push(`${ROUTES.home}?${stringify({ ...currentParams, query: value })}`);
-      dispatch(RepositoriesActions.search(value));
-    },
-    [dispatch]
-  );
+  const [handleSearch] = useDebouncedCallback(value => {
+    const currentParams = parse(history.location.search);
+    history.push(`${ROUTES.home}?${stringify({ ...currentParams, query: value })}`);
+    dispatch(RepositoriesActions.search(value));
+  }, 500);
 
-  const handleChange = useCallback(e => {
-    setValue(e.target.value);
-  }, []);
+  const handleChange = useCallback(
+    e => {
+      setValue(e.target.value);
+      handleSearch(e.target.value);
+    },
+    [handleSearch]
+  );
 
   return (
     <Container>
-      <Input.Search
-        placeholder={formatMessage(messages.placeholder)}
-        enterButton={formatMessage(messages.search)}
-        size="large"
-        value={value}
-        onChange={handleChange}
-        onSearch={handleSearch}
-      />
+      <Input placeholder={formatMessage(messages.placeholder)} size="large" value={value} onChange={handleChange} />
     </Container>
   );
 };
